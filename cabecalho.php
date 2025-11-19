bdb685c8dcdf4aaf2e8f17033e3c783b9422c460
Nethanyas <nethanyasdomingosdasilva@gmail.com>
2025-11-19 17:21:00 -0300

    Initial commit

---
+++ cabecalho.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>CRUD Contato - PHP Profissional</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+</head>
+<body>
+<div class="container">
+<div class="jumbotron">
+<h1>CRUD - Contato - PHP Profissional</h1>
+</div>
